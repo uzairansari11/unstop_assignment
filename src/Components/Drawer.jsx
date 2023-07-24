36b96b7519de40c273adb75edfc499ce006bb94c
Uzair Ansari <uzairansari11>
2023-07-25 04:28:13 +0530

    side drawer  fixed

--- src/Components/Drawer.jsx
+++ src/Components/Drawer.jsx
@@ -1,114 +1,156 @@
 import {
-  Box,
-  Button,
-  Drawer,
-  DrawerBody,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerHeader,
-  DrawerOverlay,
-  Flex,
-  Grid,
-  Text
+	Box,
+	Button,
+	Drawer,
+	DrawerBody,
+	DrawerCloseButton,
+	DrawerContent,
+	DrawerHeader,
+	DrawerOverlay,
+	Flex,
+	Grid,
+	Image,
+	Text,
 } from "@chakra-ui/react";
-import AnalyticsOutlinedIcon from "@mui/icons-material/AnalyticsOutlined";
-import DashboardOutlinedIcon from "@mui/icons-material/DashboardOutlined";
-import LibraryBooksOutlinedIcon from "@mui/icons-material/LibraryBooksOutlined";
-import NoteAltOutlinedIcon from "@mui/icons-material/NoteAltOutlined";
 import SegmentIcon from "@mui/icons-material/Segment";
 import { useRef } from "react";
-
+import assessment from "../Assests/assessment.svg";
+import dashboard from "../Assests/dashboard.svg";
+import mylibrary from "../Assests/mylibrary.svg";
+import roundstatus from "../Assests/roundstatus.svg";
 function MyDrawer({ isOpen, onOpen, onClose }) {
-  // const { isOpen, onOpen, onClose } = useDisclosure()
-  const btnRef = useRef();
+	const btnRef = useRef();
 
-  return (
-    <>
-      <Box ref={btnRef} colorScheme="teal" onClick={onOpen}>
-        <SegmentIcon
-          sx={{
-            transform: "scaleX(-1)",
-            fontSize: "40px",
-            borderRadius: "50%",
-            bgcolor: "#F2F8FE",
-            padding: "5px",
-          }}
-        />
-      </Box>
-      <Drawer
-        bgColor={"#F2F8FE"}
-        isOpen={isOpen}
-        placement="left"
-        onClose={onClose}
-        finalFocusRef={btnRef}
-      >
-        <DrawerOverlay />
-        <DrawerContent bgColor={"#F2F8FE"}  >
-          <DrawerCloseButton />
-          <DrawerHeader>Menu</DrawerHeader>
+	return (
+		<>
+			<Box ref={btnRef} colorScheme="teal" onClick={onOpen}>
+				<SegmentIcon
+					sx={{
+						transform: "scaleX(-1)",
+						fontSize: "40px",
+						borderRadius: "50%",
+						bgcolor: "#F2F8FE",
+						padding: "5px",
+					}}
+				/>
+			</Box>
+			<Drawer
+				bgColor={"#F2F8FE"}
+				isOpen={isOpen}
+				placement="left"
+				onClose={onClose}
+				finalFocusRef={btnRef}
+				size={"xs"}
+			>
+				<DrawerOverlay />
+				<DrawerContent bgColor={"#F2F8FE"}>
+					<DrawerCloseButton />
+					<DrawerHeader>Menu</DrawerHeader>
 
-          <DrawerBody>
-            <Grid
-              gap="10px"
-              p="10px 20px "
-              fontSize={"13px"}
-              fontWeight={"500"}
-            >
-              <Flex p="10px" gap="10px">
-                <DashboardOutlinedIcon fontSize="small" />
-                <Text>Dashboard</Text>
-              </Flex>
-              <Flex
-                p="10px 0px"
-                borderRadius={"10px"}
-                bgColor="#E5F1FC"
-                border="2px solid #4096EC"
-                color={"#4096EC"}
-                gap="10px"
-              >
-                <NoteAltOutlinedIcon fontSize="small" />
-                <Text>Assessment</Text>
-              </Flex>
-              <Flex p="10px" gap="10px">
-                <LibraryBooksOutlinedIcon fontSize="small" />
-                <Text>My Library</Text>
-              </Flex>
+					<DrawerBody>
+						<Grid gap="10px" p="5px" fontSize={"13px"} fontWeight={"500"}>
+							{/* 1st box  */}
+							<Flex
+								p="10px"
+								gap="10px"
+								alignItems="center"
+							>
+								<Box>
+									{" "}
+									<Image
+										src={dashboard}
+										alt="dashboard"
+										margin="auto"
+										width="25px"
+										height="25px"
+									/>
+								</Box>
 
-              <Flex
-                justifyContent={"space-between"}
-                alignItems={"center"}
-                borderTop={"1px dashed #E3E5E8"}
-              >
-                <Flex p="10px" gap="10px">
-                  <AnalyticsOutlinedIcon fontSize="small" />
-                  <Text>Round Status</Text>
-                </Flex>
-                <Box>
-                  <Button
-                    fontSize={"10px"}
-                    w="46px"
-                    h="18px"
-                    textAlign={"center"}
-                    alignItems={"center"}
-                    borderRadius={"7px"}
-                    border={"1px solid #DF623A"}
-                    display={"block"}
-                    fontWeight={"500"}
-                    color="#DF623A"
-                    p="0px 4px"
-                    bgColor={"#FBEBEA"}
-                    m={"auto"}
-                  >
-                    Admin
-                  </Button>
-                </Box>
-              </Flex>
-            </Grid>
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
-    </>
-  );
+								<Text>Dashboard</Text>
+							</Flex>
+							{/* 2nd box  */}
+							<Flex
+								p="10px"
+								gap="10px"
+								borderRadius={"10px"}
+								bgColor="#E5F1FC"
+								border="2px solid #4096EC"
+								color={"#4096EC"}
+								alignItems={"center"}
+							>
+								<Box>
+									<Image
+										src={assessment}
+										alt="dashboard"
+										margin={"auto"}
+										width="25px"
+										height="25px"
+									/>
+								</Box>
+
+								<Text>Assessment</Text>
+							</Flex>
+							{/* 3rd box */}
+
+							<Flex
+								p="10px"
+								gap="10px"
+								alignItems="center"
+								// border="1px solid red"
+							>
+								<Box>
+									<Image
+										src={mylibrary}
+										alt="dashboard"
+										margin={"auto"}
+										width="25px"
+										height="25px"
+									/>
+								</Box>
+
+								<Text>My Library</Text>
+							</Flex>
+							<Flex
+								justifyContent={"space-between"}
+								alignItems={"center"}
+								borderTop={"1px dashed #E3E5E8"}
+							>
+								<Flex p="10px" gap="10px">
+									<Image
+										src={roundstatus}
+										alt="dashboard"
+										margin={"auto"}
+										width="25px"
+										height="25px"
+									/>
+									<Text>Round Status</Text>
+								</Flex>
+								<Box>
+									<Button
+										fontSize={"10px"}
+										w="44px"
+										h="18px"
+										textAlign={"center"}
+										alignItems={"center"}
+										borderRadius={"7px"}
+										border={"1px solid #DF623A"}
+										display={"block"}
+										fontWeight={"500"}
+										color="#DF623A"
+										p="0px 4px"
+										bgColor={"#FBEBEA"}
+										m={"auto"}
+									>
+										Admin
+									</Button>
+								</Box>
+							</Flex>
+						</Grid>
+					</DrawerBody>
+				</DrawerContent>
+			</Drawer>
+		</>
+	);
 }
 
 export default MyDrawer;
